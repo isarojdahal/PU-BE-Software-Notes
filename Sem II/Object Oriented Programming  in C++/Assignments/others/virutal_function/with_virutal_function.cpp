f1cefb65496a2c6532e8c4b3d92af45cee8e19bc
Saroj Dahal <sarojdahal1213@gmail.com>
2021-07-27 17:46:17 +0545

    created: virutal_function dir and added Virutal_function related C++ Program files

---
+++ Sem II/Object Oriented Programming  in C++/Assignments/others/virutal_function/with_virutal_function.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Base
+{
+public:
+    virtual void print()
+    {
+
+        cout << "Printing from Base Class" << endl;
+    }
+};
+
+class Derived : public Base
+{
+
+public:
+    void print()
+    {
+        cout << "Printing from Derived Class" << endl;
+    }
+};
+
+int main()
+{
+
+    Base *bptr, baseObj;
+    bptr = &baseObj;
+    bptr->print();
+    //output:  Printing from Base Class
+
+    Derived dObj;
+    bptr = &dObj;
+    bptr->print();
+    //output: Printing from Base Class
+}
